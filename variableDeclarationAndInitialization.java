e15c031783d5ded4e3fe7d914aae29e4352d47e1
janu-satya-14 <2311CS010342@mallareddyuniversity.ac.in>
2025-12-16 09:59:21 +0530

    VariableDeclarationAndInitialization

---
+++ variableDeclarationAndInitialization.java
@@ -0,0 +1,20 @@
+public class variableDeclarationAndInitialization {
+    public static void main(String[] args) {
+        //declaration
+        int number;
+        double salary;
+        //initialization
+        number = 20;
+        salary = 45000.50;
+        System.out.println(number);
+        System.out.println(salary);
+        String section = "delta";
+        float marks = 90.9f;
+        long mobile = 9866719488L;
+        char mail = '@';
+        System.out.println(section);
+        System.out.println(marks);
+        System.out.println(mobile);
+        System.out.println(mail);
+    }
+}
