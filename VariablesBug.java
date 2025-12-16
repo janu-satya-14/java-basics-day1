23b3adad53b4f172f14332cbeb8fbd6244addc12
janu-satya-14 <2311CS010342@mallareddyuniversity.ac.in>
2025-12-16 10:06:15 +0530

    VariablesDemo.java

---
+++ VariablesBug.java
@@ -0,0 +1,25 @@
+//Before Debugging
+//public class VariablesBug {
+//    public static void main(String[] args) {
+//    int number="10";
+//    String name= Jhansi;
+//    int total;
+//    System.out.println(total);
+//    double marks = 86;5
+//    System.out.println("number"+number , "name"+name, "total"+total, "marks"+marks)
+//    }
+//}
+
+//After Debugging
+public class VariablesBug {
+    public static void main(String[] args) {
+        int number=10;
+        String name= "Jhansi";
+        int total=100;
+        System.out.println(total);
+        double marks = 865;
+        System.out.println("number "+number);
+        System.out.println("name "+name);
+        System.out.println("total " +total);
+    }
+}
